2be19569f108c9ae90c578118374e7f6f9bbe781
zane <zcx@stanfin.com>
2021-03-05 13:37:37 +0800

    init

---
+++ app/src/main/java/com/zane/homework/MarsRover.java
@@ -0,0 +1,13 @@
+package com.zane.homework;
+
+import java.util.Scanner;
+
+public class MarsRover {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input order");
+        String nextLine = scanner.nextLine();
+        System.out.println("output: "+nextLine);
+    }
+}
